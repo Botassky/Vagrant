b05ce6cb9ddd99d5721dd3ac8d594d1421593d18
GinUsagi <pfc.william.g.kirby@gmail.com>
2020-02-12 22:44:19 +0900

    初版（改訂）

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2020/02/12 22:27 更新',
+    VERSION: '2020/02/12 22:43 更新',
     LANGUAGE: 'ja',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
